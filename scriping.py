88ee0db66b63408a6b400fd23b8b7ce6e1ec6572
MohamedCS <you@example.com>
2021-12-19 01:57:56 +0100

    scraping description from links and refactoring code

--- scriping.py
+++ scriping.py
@@ -1,3 +1,4 @@
+from os import close
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -8,6 +9,7 @@ from itertools import zip_longest
 data_text = []
 numbers = []
 links = []
+description = []
 
 chromedriver_path= "/usr/lib/chromium-browser/chromedriver"
 driver = webdriver.Chrome(chromedriver_path)
@@ -30,6 +32,15 @@ links_zone = soup.find_all("div" ,{"class":"col-sm-6 col-md-4 col-12"})
 for i in range(len(data)):
     data_text.append(data[i].text)
 
+final_list = [data_text,numbers,description]
+
+exported = zip_longest(*final_list)
+
+with open("/home/moh/Desktop/phones.csv" ,"w") as file_phones:
+    write = csv.writer(file_phones)
+    write.writerow(["phones data" ,"number","description"])
+    write.writerows(exported)
+
 for i in range(len(links_zone)):
     links.append("https://www.ouedkniss.com"+links_zone[i].find("a").attrs["href"])
 
@@ -40,16 +51,45 @@ for link in links:
   page = driver.page_source
   driver.quit()
   soup = BeautifulSoup(page, 'html.parser')
+
   n = soup.find_all("a",{"class":"v-chip v-chip--clickable v-chip--link v-chip--no-color theme--light v-size--default"})
+
+  d = soup.find_all("div",{"class":"__description mb-4"})
+  
   for i in range(len(n)):
-    numbers.append(n[i].attrs["href"])
-    print(numbers)
+    # print(n[i])
+    tempnumber = n[i].attrs["href"]
+    numbers.append(tempnumber)
+    
+    final_list = [data_text,numbers,description]
+    exported = zip_longest(*final_list)
 
-final_list = [data_text,numbers]
+    with open("/home/moh/Desktop/phones.csv" ,"w") as file_phones:
+     write = csv.writer(file_phones)
+     write.writerow(["phones data" ,"number","description"])
+     write.writerows(exported)
+     break
 
-exported = zip_longest(*final_list)
+  if len(d) == 0 :
+     description.append("null")
+     final_list = [data_text,numbers,description]
+     exported = zip_longest(*final_list)
 
-with open("/home/moh/Desktop/phones.csv" ,"w") as file_phones:
-    write = csv.writer(file_phones)
-    write.writerow(["phones data" ,"number"])
-    write.writerows(exported)
+     with open("/home/moh/Desktop/phones.csv" ,"w") as file_phones:
+      write = csv.writer(file_phones)
+      write.writerow(["phones data" ,"number","description"])
+      write.writerows(exported)
+
+  for i in range(len(d)):
+    print(d[i].find("div").text)
+    tempdes = d[i].find("div").text
+    description.append(tempdes)
+    
+    final_list = [data_text,numbers,description]
+    exported = zip_longest(*final_list)
+
+    with open("/home/moh/Desktop/phones.csv" ,"w") as file_phones:
+     write = csv.writer(file_phones)
+     write.writerow(["phones data" ,"number","description"])
+     write.writerows(exported)
+     break
